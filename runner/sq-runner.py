fcc2b530aca7782bad430e91bd5c6102d6856d2d
Giovanni Fulco <giovanni.fulco@gmail.com>
2026-04-05 23:25:18 +0200

    Squeezelite: fix command line

--- runner/sq-runner.py
+++ runner/sq-runner.py
@@ -46,12 +46,12 @@ class CommandLineOptionMapperData:
             cmd_line_option: str,
             dflt_value: str = None,
             boolean_value: bool = False,
-            in_quotes: bool = False):
+            replace_spaces_with_colon: bool = False):
         self.__var_name: str = var_name
         self.__cmd_line_option: str = cmd_line_option
         self.__dflt_value: str = dflt_value
         self.__boolean_value: bool = boolean_value
-        self.__in_quotes: bool = in_quotes
+        self.__replace_spaces_with_colon: bool = replace_spaces_with_colon
 
     @property
     def var_name(self) -> str:
@@ -70,8 +70,8 @@ class CommandLineOptionMapperData:
         return self.__boolean_value
 
     @property
-    def in_quotes(self) -> bool:
-        return self.__in_quotes
+    def replace_spaces_with_colon(self) -> bool:
+        return self.__replace_spaces_with_colon
 
 
 class LauncherOptionData:
@@ -105,86 +105,71 @@ class CommandLineOptionMapper(Enum):
     SQUEEZELITE_TIMEOUT = CommandLineOptionMapperData(
         var_name=VariableName.SQUEEZELITE_TIMEOUT.value,
         cmd_line_option="-C",
-        dflt_value=5)
+        dflt_value="5")
     SQUEEZELITE_LINEAR_VOLUME = CommandLineOptionMapperData(
         var_name=VariableName.SQUEEZELITE_LINEAR_VOLUME.value,
         cmd_line_option="-X",
         boolean_value=True)
     SQUEEZELITE_NAME = CommandLineOptionMapperData(
         var_name=VariableName.SQUEEZELITE_NAME.value,
-        cmd_line_option="-n",
-        in_quotes=True)
+        cmd_line_option="-n")
     SQUEEZELITE_MODEL_NAME = CommandLineOptionMapperData(
         var_name=VariableName.SQUEEZELITE_MODEL_NAME.value,
-        cmd_line_option="-M",
-        in_quotes=True)
+        cmd_line_option="-M")
     SQUEEZELITE_PARAMS = CommandLineOptionMapperData(
         var_name=VariableName.SQUEEZELITE_PARAMS.value,
         cmd_line_option="-a",
-        in_quotes=True)
+        replace_spaces_with_colon=True)
     SQUEEZELITE_BUFFER_SIZE = CommandLineOptionMapperData(
         var_name=VariableName.SQUEEZELITE_BUFFER_SIZE.value,
         cmd_line_option="-b",
-        in_quotes=True)
+        replace_spaces_with_colon=True)
     SQUEEZELITE_VOLUME_CONTROL = CommandLineOptionMapperData(
         var_name=VariableName.SQUEEZELITE_VOLUME_CONTROL.value,
-        cmd_line_option="-V",
-        in_quotes=True)
+        cmd_line_option="-V")
     SQUEEZELITE_UNMUTE = CommandLineOptionMapperData(
         var_name=VariableName.SQUEEZELITE_UNMUTE.value,
-        cmd_line_option="-U",
-        in_quotes=True)
+        cmd_line_option="-U")
     SQUEEZELITE_VISUALIZER = CommandLineOptionMapperData(
         var_name=VariableName.SQUEEZELITE_VISUALIZER.value,
-        cmd_line_option="-v",
-        in_quotes=True)
+        cmd_line_option="-v")
     SQUEEZELITE_MAC_ADDRESS = CommandLineOptionMapperData(
         var_name=VariableName.SQUEEZELITE_MAC_ADDRESS.value,
         cmd_line_option="-m",
-        in_quotes=True)
+        replace_spaces_with_colon=True)
     SQUEEZELITE_REPORT_MAX_SAMPLE_RATE = CommandLineOptionMapperData(
         var_name=VariableName.SQUEEZELITE_REPORT_MAX_SAMPLE_RATE.value,
-        cmd_line_option="-Z",
-        in_quotes=True)
+        cmd_line_option="-Z")
     SQUEEZELITE_CODECS = CommandLineOptionMapperData(
         var_name=VariableName.SQUEEZELITE_CODECS.value,
-        cmd_line_option="-c",
-        in_quotes=True)
+        cmd_line_option="-c")
     SQUEEZELITE_EXCLUDE_CODECS = CommandLineOptionMapperData(
         var_name=VariableName.SQUEEZELITE_EXCLUDE_CODECS.value,
-        cmd_line_option="-e",
-        in_quotes=True)
+        cmd_line_option="-e")
     SQUEEZELITE_UPSAMPLING = CommandLineOptionMapperData(
         var_name=VariableName.SQUEEZELITE_UPSAMPLING.value,
         cmd_line_option="-u",
-        dflt_value="E",
-        in_quotes=True)
+        dflt_value="E")
     SQUEEZELITE_RATES = CommandLineOptionMapperData(
         var_name=VariableName.SQUEEZELITE_RATES.value,
-        cmd_line_option="-r",
-        in_quotes=True)
+        cmd_line_option="-r")
     SQUEEZELITE_DELAY = CommandLineOptionMapperData(
         var_name=VariableName.SQUEEZELITE_DELAY.value,
         cmd_line_option="-D",
-        dflt_value="500",
-        in_quotes=True)
+        dflt_value="500")
     SQUEEZELITE_PRIORITY = CommandLineOptionMapperData(
         var_name=VariableName.SQUEEZELITE_PRIORITY.value,
         cmd_line_option="-p",
-        dflt_value=45,
-        in_quotes=False)
+        dflt_value=45)
     SQUEEZELITE_READ_FORMATS_FROM_HEADER = CommandLineOptionMapperData(
         var_name=VariableName.SQUEEZELITE_READ_FORMATS_FROM_HEADER.value,
-        cmd_line_option="-W",
-        in_quotes=True)
+        cmd_line_option="-W")
     SQUEEZELITE_POWER_SCRIPT = CommandLineOptionMapperData(
         var_name=VariableName.SQUEEZELITE_POWER_SCRIPT.value,
-        cmd_line_option="-S",
-        in_quotes=True)
+        cmd_line_option="-S")
     SQUEEZELITE_RPI_GPIO = CommandLineOptionMapperData(
         var_name=VariableName.SQUEEZELITE_RPI_GPIO.value,
-        cmd_line_option="-G",
-        in_quotes=True)
+        cmd_line_option="-G")
 
     @property
     def var_name(self) -> str:
@@ -203,8 +188,8 @@ class CommandLineOptionMapper(Enum):
         return self.value.boolean_value
 
     @property
-    def in_quotes(self) -> bool:
-        return self.value.in_quotes
+    def replace_spaces_with_colon(self) -> bool:
+        return self.value.replace_spaces_with_colon
 
 
 class LauncherOption(Enum):
@@ -264,8 +249,10 @@ def add_command_line_option(command_line: list[str], mapper: CommandLineOptionMa
         command_line += [ mapper.cmd_line_option ]
     elif not mapper.boolean_value and v:
         print(f"Using [{v}] for parameter [{mapper.cmd_line_option}] ...")
-        mapped_value: str = v if not mapper.in_quotes else f"\"{v}\""
-        command_line += [ mapper.cmd_line_option, str(mapped_value).replace(" ", ":") ]
+        mapped_value: str = v
+        if mapper.replace_spaces_with_colon:
+            mapped_value = mapped_value.replace(" ", ":")
+        command_line += [ mapper.cmd_line_option, mapped_value ]
     return command_line
 
 
